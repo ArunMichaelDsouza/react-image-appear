203364fadd7209ebdb883020fad0fb1c6c93c213
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-04 00:17:39 +0530

    Image load event handler added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import React, { Component } from 'React';
 class ReactImageAppear extends Component {
     constructor(props) {
         super(props);
+
+        this.onLoad = this.onLoad.bind(this);
+    }
+
+    onLoad() {
+        console.log('loaded');
     }
 
     render() {
-        return <img />
+        return <img onLoad={this.onLoad} />
     }
 }
 
