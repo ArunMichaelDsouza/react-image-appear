2d06eb3a97fe2d3b071194f0c69b3d42a21fe258
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-04 00:18:33 +0530

    Interval to check for image dimensions added

--- src/index.js
+++ src/index.js
@@ -7,12 +7,25 @@ class ReactImageAppear extends Component {
         this.onLoad = this.onLoad.bind(this);
     }
 
+    componentDidMount() {
+        const img = this.refs.img;
+        console.log('mounted');
+
+        var styleInterval = setInterval(function () {
+            if (Number(window.getComputedStyle(img).width.match(/[0-9]+/)) && Number(window.getComputedStyle(img).height.match(/[0-9]+/))) {
+                clearInterval(styleInterval);
+                console.log(window.getComputedStyle(img).width);
+                console.log(window.getComputedStyle(img).height);
+            }
+        }, 10);
+    }
+
     onLoad() {
         console.log('loaded');
     }
 
     render() {
-        return <img onLoad={this.onLoad} />
+        return <img onLoad={this.onLoad} ref="img" src="https://newevolutiondesigns.com/images/freebies/tropical-beach-background-8.jpg" />
     }
 }
 
