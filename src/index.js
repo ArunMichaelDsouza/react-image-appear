9dfed53b01342649320ab28d12978ca4f4bbec19
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-03 22:40:53 +0530

    Component bare-bones added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'React';
+
+class ReactImageAppear extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <img />
+    }
+}
+
+export default ReactImageAppear;
