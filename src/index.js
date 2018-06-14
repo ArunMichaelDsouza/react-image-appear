d5022f19c7153073496a1924985e7d84925e18b2
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-15 03:38:43 +0530

    Image element dynamically created by react and rendered in DOM

--- src/index.js
+++ src/index.js
@@ -5,40 +5,57 @@ const parseComputedDimensions = el => {
         width: Number(window.getComputedStyle(el).width.match(/\d+/)),
         height: Number(window.getComputedStyle(el).height.match(/\d+/))
     };
+}, createContainer = (el, width, height) => {
+    return <div style={{ width, height, background: '#aaa' }}>{el}</div>
 };
+
+const onLoad = function () {
+    console.log('loaded');
+}
 
 class ReactImageAppear extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            img: null
+        };
+
         this.getImageDimensions = this.getImageDimensions.bind(this);
-        this.onLoad = this.onLoad.bind(this);
     }
 
     componentDidMount() {
-        const { img: imgEl } = this.refs;
+        const { src } = this.props;
 
-        this.getImageDimensions(imgEl);
+        this.setState({ img: React.createElement('img', { src, onLoad }) });
     }
 
     getImageDimensions(imgEl) {
         const dimensionsInterval = setInterval(() => {
             const { width, height } = parseComputedDimensions(imgEl);
 
+            console.log(width, height);
             if (width && height) {
                 clearInterval(dimensionsInterval);
                 console.log('interval cleared');
                 console.log(width, height);
+                this.setState({ container: createContainer(imgEl, width, height) });
             }
         }, 10);
     }
 
-    onLoad() {
-        console.log('loaded');
-    }
-
     render() {
-        return <img onLoad={this.onLoad} ref="img" src="https://newevolutiondesigns.com/images/freebies/tropical-beach-background-8.jpg" />
+        const { img } = this.state;
+
+        return img;
+    }
+}
+
+class Greeting extends React.Component {
+    render() {
+        return (
+            <ReactImageAppear src="https://newevolutiondesigns.com/images/freebies/tropical-beach-background-8.jpg" />
+        );
     }
 }
 
