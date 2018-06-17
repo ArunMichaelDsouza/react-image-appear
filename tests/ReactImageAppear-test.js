98f608312b5f49336d508e43cf3cb358025858ac
ArunMichaelDsouza <amdsouza92@gmail.com>
2018-06-18 02:33:48 +0530

    Module's index file renamed

--- tests/ReactImageAppear-test.js
+++ tests/ReactImageAppear-test.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import ReactImageAppear from '../src/index';
+import ReactImageAppear from '../src/ReactImageAppear';
 
 describe('ReactImageAppear Component', () => {
     it('should render without throwing an error', () => {
